f03f5a292c4b209c98f25905b3fc0ec2402a598d
bkthomps <bkthomps@edu.uwaterloo.ca>
2019-04-12 01:49:18 -0700

    Make errors obvious

--- create_password.sh
+++ create_password.sh
@@ -24,44 +24,44 @@ size=$5
 
 if [[ $allow_lowercase -ne 0 && $allow_lowercase -ne 1 ]]
 then
-	echo "allow_lowercase must be 0 or 1"
+	echo "Error: allow_lowercase must be 0 or 1"
 	exit 1
 fi
 
 if [[ $allow_uppercase -ne 0 && $allow_uppercase -ne 1 ]]
 then
-        echo "allow_uppercase must be 0 or 1"
+        echo "Error: allow_uppercase must be 0 or 1"
         exit 1
 fi
 
 if [[ $allow_numbers -ne 0 && $allow_numbers -ne 1 ]]
 then
-        echo "allow_numbers must be 0 or 1"
+        echo "Error: allow_numbers must be 0 or 1"
         exit 1
 fi
 
 if [[ $allow_special_characters -ne 0 && $allow_special_characters -ne 1 ]]
 then   
-        echo "allow_special_characters must be 0 or 1"
+        echo "Error: allow_special_characters must be 0 or 1"
         exit 1
 fi
 
 regex='^[0-9]+$'
 if ! [[ $size =~ $regex ]]
 then
-	echo "size must be a number"
+	echo "Error: size must be a number"
 	exit 1 
 fi
 
 if [[ $allow_lowercase -eq 0 && $allow_uppercase -eq 0 && $allow_numbers -eq 0 && $allow_special_characters -eq 0 ]]
 then
-	echo "no characters are allowed... nothing can be generated"
+	echo "Error: no characters are allowed... nothing can be generated"
 	exit 1
 fi
 
 if [[ $size -lt 1 ]]
 then
-	echo "size is less than 1... nothing can be generated"
+	echo "Error: size is less than 1... nothing can be generated"
 	exit 1
 fi
 
@@ -90,6 +90,7 @@ fi
 char_set_length=${#char_set}
 
 code=""
+
 for i in `seq 1 $size`
 do
 	index=$(($RANDOM % $char_set_length))
@@ -100,4 +101,6 @@ done
 echo "$code" | pbcopy
 
 echo "Your clipboard now contains the randomly generated passcode"
+
+exit 0
 
